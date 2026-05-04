2292867636563225c1dd97465f92f185b5993115
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-04 11:29:12 -0600

    planning for next ex

--- bytes_and_big_integers.py
+++ bytes_and_big_integers.py
@@ -1,1 +1,4 @@
 # Link: https://cryptohack.org/courses/intro/enc4/
+# Objective: We'd like to encrypt a string messenger, but RSA and other crypto algos only work on
+# numbers.
+# Thus: Ordinal bytes -> Hex -> Concat 
