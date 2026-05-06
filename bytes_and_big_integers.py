308594fb5ba0dff0d539d531b27612ec4e4b9073
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-06 10:29:46 -0600

    WIP challenge

--- bytes_and_big_integers.py
+++ bytes_and_big_integers.py
@@ -1,4 +1,5 @@
 # Link: https://cryptohack.org/courses/intro/enc4/
 # Objective: We'd like to encrypt a string messenger, but RSA and other crypto algos only work on
 # numbers.
-# Thus: Ordinal bytes -> Hex -> Concat 
+# Thus: Ordinal bytes -> Hex -> Concat
+input = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
