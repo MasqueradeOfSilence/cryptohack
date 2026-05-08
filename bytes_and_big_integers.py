8314e471e78c9213c38ba0b595f50b9ded657d4d
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-08 11:50:25 -0600

    WIP, not done

--- bytes_and_big_integers.py
+++ bytes_and_big_integers.py
@@ -1,5 +1,9 @@
 # Link: https://cryptohack.org/courses/intro/enc4/
+from Crypto.Util.number import *
 # Objective: We'd like to encrypt a string messenger, but RSA and other crypto algos only work on
 # numbers.
 # Thus: Ordinal bytes -> Hex -> Concat
+
+# base10?
 input = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+# bytes_conversion = long_to_bytes(input)
