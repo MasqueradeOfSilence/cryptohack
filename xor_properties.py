74e0f4eac715609dd28a1bd3489c9bf57d2aa700
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-19 11:12:40 -0600

    working on xor

---
+++ xor_properties.py
@@ -0,0 +1,6 @@
+import operator
+key1 = "a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313"
+key2_xor_with_key1 = "37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e"
+key2_xor_with_key3 = "c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1"
+result_of_xor_flag_with_keys = "04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf"
+# Operator has a .xor method, but decode from hex to bytes first
